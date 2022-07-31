3ade6f8ae788bf08d2d95e3b9a79e8f7221b2174
spykid-111 <rrsingh1101@gmail.com>
2022-07-31 16:20:20 +0530

    jquery done

--- app.js
+++ app.js
@@ -15,3 +15,36 @@ $('h1').addClass("bold background")
 // $(<CSS SELECTOR>).removeClass("<class name>")
 $('h1').removeClass("bold")
 // .hasClass() is used to check if the class exist or not, returns true/false accordingly
+
+
+
+// to change the text/html
+// .text() method is used 
+$("h1").text("Bye")
+//.html() is used 
+
+
+
+//for adding attributes
+$('h1').attr("id","orange");
+
+
+//Event Listeners
+$('button').click(()=>{
+    $('h1').css("color","pink");
+})
+$('button').dblclick(()=>{
+    $('h1').css("color","purple");
+})
+$('input').keypress((event)=>{
+    $("h1").text(event.key);
+})
+
+// for all events use .on()method 
+$("button").on("mouseover",()=>{
+    $("h1").text("Mouse")
+})
+
+
+
+// USE DOCS FOR FURTHER METHODS
